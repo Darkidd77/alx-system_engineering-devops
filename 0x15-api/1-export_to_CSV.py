96d8db5b29e71d3cd746379af84f68bc2899a8b5
darkidd77 <m.almahdi77@yahoo.com>
2024-05-20 20:20:20 +0100

    task-1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-""" Export api to csv"""
+""" Export API to CSV"""
 import csv
 import requests
 import sys
 
 if __name__ == '__main__':
-    user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    if len(sys.argv) != 2:
+        print("Usage: {} <user_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user_id
     res = requests.get(url_user)
-    """EVERYTHING"""
+
+    if res.status_code != 200:
+        print("User ID not found.")
+        sys.exit(1)
+
     user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
+    "EVERYTHING"
+    url_tasks = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    res = requests.get(url_tasks)
     tasks = res.json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in tasks:
+            "Complete"
             completed = task.get('completed')
-            """Complete"""
+            "Done"
             title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
+            csv_writer.writerow([user_id, user_name, completed, title_task])
