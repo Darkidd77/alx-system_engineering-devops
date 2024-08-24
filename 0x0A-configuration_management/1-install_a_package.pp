4c752d643c0ff8e459c4f402726ae39bb255da7f
darkidd77 <m.almahdi77@yahoo.com>
2024-08-24 18:45:06 +0100

    task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #install flask and pip with puppet
 
-package { 'python3.8':
+package { 'Python3.8':
   ensure   => '3.8.10',
   provider => 'pip3',
 }
@@ -9,7 +9,6 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
 
 package { 'werkzeug':
   ensure   => '2.1.1',
